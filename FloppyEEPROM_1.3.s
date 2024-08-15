392ab0934983622400156295702c3e1e529d66b5
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2024-08-14 23:06:45 -0700

    Add RAM sizing code and finish patch tables

--- FloppyEEPROM_1.3.s
+++ FloppyEEPROM_1.3.s
@@ -41,6 +41,20 @@ PlusPatchTbl:
             dc.w    $4000CE-BaseOfROM
             dc.w    PatchPlusBoot2-PtchROMBase
             dc.w    $4003A8-BaseOfROM 
+            dc.w    PatchBootRetry-PtchROMBase
+            dc.w    $400594-BaseOfROM
+            dc.w    PatchCPUFlag-PtchROMBase
+            dc.w    $4005BC-BaseOfROM
+            dc.w    PatchGetPRAM-PtchROMBase
+            dc.w    $400972-BaseOfROM
+            dc.w    PatchGNEFilter-PtchROMBase
+            dc.w    $40087E-BaseOfROM
+            dc.w    PatchPlusInitIOMgr-PtchROMBase
+            dc.w    $400894-BaseOfROM
+            dc.w    PatchInitIOMgr-PtchROMBase
+            dc.w    $4008A2-BaseOfROM
+            dc.w    PatchInitIOMgr2-PtchROMBase
+            
             org     $F80080
             dc.l    TROMCode
             dc.l    WarmEntry
@@ -76,23 +90,144 @@ ColdEntry:
             or.b    D0,($0,A0)
             move.w  #32768/4-1,D0
             moveq   #-1,D1
-WarmEntry:
+            movea.l #OutboundDisp+32768,A0
+.ClearScreenLoop:
+            move.l  D1,-(A0)
+            dbf     D0,.ClearScreenLoop
+            move.l  SP,$707D00
+            movea.l #OutboundDisp+32768,SP
+            bsr.w   DrawWallaby
+            move.l  #262144,D0
+.DelayLoop:
+            subq.l  #1,D0
+            bne.b   .DelayLoop
+            move.w  #369,D0
+            clr.w   D1
+            movea.l #$707D1C,A0
+.Loop3:
+            move.w  D1,(A0)+
+            dbf     D0,.Loop3
+            movea.l #OutboundDisp+32768,SP
+            clr.w   OutboundCfg
+            cmpa.l  #$400062,A1                     ; Mac Plus return address in A1?
+            bne.b   .IsSE
+            move.l  #$FE000,$707D00
+            addq.l  #4,A1                           ; Skip past setting the Status Register in the Plus ROM
+            movea.l #PlusPatchTbl,A6
+            bra.b   .LoadFirstPatchLocation
+.IsSE:
+            bset.b  #IsMacSEROM,OutboundCfg
+            movea.l #$401C04,A1
+            movea.l #SEPatchTbl,A6
+.LoadFirstPatchLocation:
+            move.w  (A6)+,ExpectedPC
+            move.w  (A6)+,PatchOffset
+            move.l  A6,PatchTblPtr
+            move.l  #PtchROMBase,PtchTblBase
+            move.w  BaseOfROM,D0
+            bsr.w   RamSizing
+            btst.b  #3,$E7E1FE
+            beq.b   .L7
+            movea.l #$580800,A0
+            movep.w ($0,A0),D0
+            cmpi.w  #$55AA,D0
+            beq.b   .L5
+            cmpi.w  #$AA55,D0
+            beq.b   .L5
+            bset.b  #CfgBit7,OutboundCfg
+            cmpi.w  #$4BB4,D0
+            beq.b   .L7
+            cmpi.w  #$4558,D0
+            beq.b   .L7
+            bclr.b  #CfgBit7,OutboundCfg
+            ori.b   #1<<CfgBit3|1<<CfgBit1,OutboundCfg
+            move.l  #300000,D0
+.L3:
+            subq.w  #1,D0
+            bne.b   .L3
+            move.b  #1,$500009
+            clr.w   SCSIBase
+            moveq   #-1,D1
+            move.w  #512*342/8/4-1,D0
+            movea.l #ScreenLow,A0
+.L4:
+            move.l  D1,(A0)+
+            dbf     D0,.L4
+            lea     SCSIBase,A0
+
+            
+
+
+PatchPlusBoot2:
 PatchMinorStartTest:
 PatchMinorStartTest2:
 PatchLoadExceptionVectors:
 InitPatch:
 PatchBeep:
-PatchBootRetry:
-PatchGetPRAM:
-PatchSetupSysAppZone:
-PatchBootRetry2:
-PatchInitADB:
-PatchInitIOMgr:
-PatchInitIOMgr2:
-PatchDrawBeepScreen:
 PatchPlusBoot:
 PatchClkNoMem:
-PatchPlusBoot2:
+WarmEntry:
+PatchBootRetry:
 PatchCPUFlag:
+PatchGetPRAM:
+PatchSetupSysAppZone:
+PatchInitADB:
 PatchGNEFilter:
 PatchPlusInitIOMgr:
+        moveq #$40,D0
+        rte
+PatchInitIOMgr:
+PatchBootRetry2:
+PatchInitIOMgr2:
+PatchDrawBeepScreen:
+RamSizing:
+        suba.l  A0,A0
+        move.b  #1,$50000B
+        clr.b   $50000D
+        moveq   #4*2,D0                             ; Test 4MB
+        bsr.b   RamMirrorCheck
+        bne.b   .Exit
+        move.b  #1,$50000D
+        moveq   #2*2+1,D0                           ; Test 2.5MB
+        bsr.b   RamMirrorCheck
+        bne.b   .Exit
+        clr.b   $50000B
+        moveq   #2*2,D0                             ; Test 2MB
+        bsr.b   RamMirrorCheck
+        bne.b   .Exit
+        clr.b   $50000D
+        moveq   #1*2,D0                             ; Test 1MB
+        bsr.b   RamMirrorCheck
+.Exit:
+        rts
+RamMirrorCheck:
+        move.l  D0,D1
+        move.w  D0,D2
+        swap    D1
+        lsl.l   #3,D1
+.WriteValues:
+        subi.l  #512*1024,D1
+        move.w  D2,(A0,D1)
+        subq.w  #1,D2
+        bne.b   .WriteValues
+        move.l  D0,D1
+        move.w  D0,D2
+        swap    D1
+        lsl.l   #3,D1
+.CheckValues:
+        subi.l  #512*1024,D1
+        cmp.w   (A0,D1),D2
+        bne.b   .FailExit
+        subq.w  #1,D2
+        bne.b   .CheckValues
+        tst.w   D0
+        rts
+.FailExit:
+        moveq   #0,D0
+        rts
+
+
+
+
+
+
