fc81be82c3bb034707fe9d20fadc1c7293cff52c
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2024-08-16 15:38:53 -0700

    Add some floppy driver code

--- FloppyEEPROM_1.3.s
+++ FloppyEEPROM_1.3.s
@@ -58,8 +58,8 @@ PlusPatchTbl:
             org     $F80080
             dc.l    TROMCode
             dc.l    WarmEntry
-            dc.b    5
-            dc.b    '1','.','3','b','2'             ; ROM version string
+            dc.b    5                               ; Length byte
+            dc.b    "1.3b2"                        ; ROM version string
             dc.b    0,0,1,3
 NewTraceVector:
             move.w  D0,-(SP)                        ; Save D0
@@ -156,7 +156,7 @@ ColdEntry:
             lea     SCSI_Base,A0
             move.b  (sICR,A0),D0
             or.b    (sCSR,A0),D0
-            andi.b   #%10000000,D0
+            andi.b  #%10000000,D0
             bne.b   .L6
             move.b  #%10000000,(sICR,A0)
             move.b  (sICR,A0),D0
@@ -429,9 +429,269 @@ RamMirrorCheck:
             moveq   #0,D0
             rts
 DrawWallaby:
+            lea     .WallabyBitmap,A0
+            movea.l #OutboundDisp+9335,A4
+.L1:
+            move.w  #7,D2
+            clr.w   D3
+            adda.w  #289,A4
+.L2:
+            move.b  (A0)+,D1
+            add.b   D1,D3
+            beq.b   .Return
+.L3:
+            bchg.b  D2,(A4)
+            bsr.b   .ReturnCheck
+            subq.b  #1,D1
+            bne.b   .L3
+            cmpi.b  #-1,D3
+            beq.b   .L1
+            move.b  (A0)+,D1
+            add.b   D1,D3
+.L4:
+            bsr.b   .ReturnCheck
+            subq.b  #1,D1
+            bne.b   .L4
+            bra.b   .L2
+.ReturnCheck:
+            dbf     D2,.Return
+            move.w  #7,D2
+            addq.w  #1,A4
+.Return:
+            rts
+.WallabyBitmap:
+RAMDisk_Driver:
+            dc.w    $4F00
+            dc.w    $0
+            dc.w    $0
+            dc.w    $0
+            dc.w    RAMDisk_Driver-RAMDisk_Open
+            dc.w    RAMDisk_Driver-RAMDisk_Prime
+            dc.w    RAMDisk_Driver-RAMDisk_Ctl
+            dc.w    RAMDisk_Driver-RAMDisk_Status
+            dc.w    RAMDisk_Driver-RAMDisk_Close
+RAMDisk_Name:
+            dc.b    5
+            dc.b    ".RAMd"
+RAMDisk_Open:
+RAMDisk_Close:
+RAMDisk_Prime:
+RAMDisk_Ctl:
+RAMDisk_Status:
+Super_Install:
+            movem.l A6-A0/D7-D0,-(SP)
+            movea.l $707D00,A1
+            lea     ($140,A1),A0
+            move.l  A0,($26,A1)
+            moveq   #-5,D0
+            btst.b  #CfgBit3,OutboundCfg
+            beq.b   .L1
+            moveq   #-49,D0
+.L1:
+            _DrvrInstall
+            lea     Super_Driver,A1
+            movea.l UTableBase,A0
+            btst.b  #CfgBit3,OutboundCfg
+            bne.b   .L2
+            movea.l ($10,A0),A0
+            bra.b   .L3
+.L2:
+            move.l  ($C0,A0),A0
+.L3:
+            movea.l (A0),A0
+            move.l  A1,(A0)+
+            move.w  (A1),(A0)+
+            suba.w  #50,SP
+            movea.l SP,A0
+            clr.b   ($1B,A0)
+            lea     Super_Name,A1
+            move.l  A1,($12,A0)
+            clr.l   ($C,A0)
+            _Open
+            adda.w  #50,SP
+            movem.l (SP)+,D0-D7/A0-A6
+            rts
+Super_Open:
+            movem.l A6-A0/D7-D1,-(SP)
+            move.l  A0,-(SP)
+            move.l  A1,-(SP)
+            move.l  A0,-(SP)
+            jsr     Super_Unknown5
+            addq.l  #8,SP
+            movea.l (SP)+,A0
+            move.w  D0,($10,A0)
+            movem.l (SP)+,D1-D7/A0-A6
+            rts
+Super_Close:
+            clr.w   ($10,A0)
+            clr.w   D0
+            rts
+Super_Prime:
+            movem.l A6-A0/D7-D1,-(SP)
+            move.l  A1,-(SP)
+            move.l  A0,-(SP)
+            movea.l $707D00,A0
+            addq.w  #1,($34,A0)
+            jsr     Super_Unknown3
+            ; Fall-through
 
+Super_Unknown2:
+            addq.l  #8,SP
+            movea.l $707D00,A0
+            subq.w  #1,($34,A0)
+            movem.l (SP)+,D1-D7/A0-A6
+            move.w  D1,($10,A0)
+            move.w  ($6,A0),D1
+            btst.l  #9,D1
+            bne.b   .Exit
+            move.l  JIODone,-(SP)
+.Exit:
+            move.w  ($10,A0),D1
+            rts
+Super_Ctl:
+            movem.l A6-A0/D7-D1,-(SP)
+            move.l  A1,-(SP)
+            move.l  A0,-(SP)
+            movea.l $707D00,A0
+            addq.w  #1,($34,A0)
+            jsr     Super_Unknown4
+            bra.b   Super_Unknown2
+Super_Status:
+            movem.l A6-A0/D7-D1,-(SP)
+            move.l  A1,-(SP)
+            move.l  A0,-(SP)
+            movea.l $707D00,A0
+            addq.w  #1,($34,A0)
+            jsr     Super_Unknown1
+            bra.b   Super_Unknown2
+Super_Driver:
+            dc.w    $4F00                           ; Flags
+            dc.w    $0                              ; Number of ticks between systask calls
+            dc.w    $0                              ; Even mask
+            dc.w    $0                              ; Driver menu ID
+            dc.w    Super_Driver-Super_Open         ; Open routine offset
+            dc.w    Super_Driver-Super_Prime        ; Prime routine offset
+            dc.w    Super_Driver-Super_Ctl          ; Control routine offset
+            dc.w    Super_Driver-Super_Status       ; Status routine offset
+            dc.w    Super_Driver-Super_Close        ; Close rotine offset
+Super_Name:
+            dc.b    6                               ; Length byte
+            dc.b    ".Super"                        ; Driver name
+            dc.b    0,0,0
+Super_UnknownData:
+            ;incbin
+            dc.b    21                              ; Length byte
+            dc.b    "Outbound Floppy Drive"
+            dc.b    0,0
+Super_UnknownData2:
+            ;incbin
 
-
-
-
+Super_Unknown6:
+            movem.l A6-A0/D7-D0,-(SP)
+            movea.l #$B00003,A0
+            lea     .Super_UnknownData3,A1
+            move.b  (A1)+,(A0)
+            move.b  (A1)+,(A0)
+            move.w  #50,D0
+.DelayLoop:
+            dbf     D0,.DelayLoop
+            move.w  #5,D0
+.L1:
+            move.b  (A1)+,(A0)
+            dbf     D0,.L1
+            movea.l #$B00001,A0
+            lea     .Super_UnknownData4,A1
+            move.w  #11,D0
+.L2:
+            move.b  (A1)+,(A0)
+            dbf     D0,.L2
+            bset.b  #5,$E0E7FE
+            bset.b  #5,$E0FFFE
+            movem.l (SP)+,D0-D7/A0-A6
+            rts
+.Super_UnknownData3:
+            dc.b    $9,$C0,$0B,$16,$5,$60,$F,$0
+.Super_UnknownData4:
+            dc.b    $4,$10,$3,$C0,$6,$AB,$7,$55,$5,$62,$F,$0
+Super_Unknown7:
+            movem.l A6-A0/D7-D0,-(SP)
+            move.l  #538,D0
+            _NewPtrSys
+            movea.l (64,SP),A1
+            move.l  A0,(A1)
+            lea     Super_UnknownData,A1
+            suba.l  PtchTblBase,A1
+            adda.l  #PtchROMBase,A1
+            jsr     Super_Unknown8
+            jsr     Super_Unknown8
+            move.w  #24,D0
+.L1:
+            move.b  (A1)+,(A0)+
+            dbf     D1,.L1
+            movem.l (SP)+,D0-D7/A0-A6
+            rts
+Super_Unknown9:
+            movem.l A6-A0/D7-D0,-(SP)
+            lea     Super_UnknownData5,A0
+            lea     Super_UknownData,A1
+            movea.l A1,A2
+            move.w  #255,D0
+.L1:
+            move.b  #$80,(A2)+
+            subq.w  #1,D0
+            bne.b   .L1
+            moveq   #63,D2
+.L2:
+            move.b  (A0,D2),D0
+            move.b  D2,(A1,D0)
+            dbf     D2,.L2
+            move.b  #$55,($55,A1)
+            movem.l (SP)+,D0-D7/A0-A6
+            rts
+Super_Unknown10:
+            movem.l A6-A0/D7-D0,-(SP)
+            movea.l $707D00,A0
+            move.w  #$14,($22,A0)
+            tst.w   ($34,A0)
+            bne.b   .Exit
+            tst.w   ($38,A0)
+            bne.b   .L1
+            moveq   #0,D0
+            move.l  D0,-(SP)
+            jsr     Super_Unknown11
+            addq.l  #4,SP
+            bra.b   .Exit
+.L1:
+            clr.w   ($38,A0)
+.Exit:
+            movem.l (SP)+,D0-D7/A0-A6
+            rts
+Super_Unknown12:
+            link.w  A6,#0
+            movem.l A0/D2-D1,-(SP)
+            movea.l ($8,A6),A0
+            move.w  ($E,A6),D0
+            subq.w  #1,D0
+.L1:
+            move.l  #100000,D1
+.L2:
+            move.b  $C80018,D2
+            bmi.b   .L4
+            btst.l  #5,D2
+            bne.b   .L3
+            subq.l  #1,D1
+            bne.b   .L2
+.L3:
+            moveq   #0,D0
+            bra.b   .L5
+.L4:
+            btst.l  #6,D2
+            bne.b   .L3
+            move.b  (A0)+,$C8001A
+            dbf     D0,.L1
+            moveq   #1,D0
+.L5:
+            movem.l (SP)+,D1-D2/A0
+            unlk    A6
+            rts
 
