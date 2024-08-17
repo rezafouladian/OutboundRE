d57d46654feeae4f7bf5b2edf76092de5e068780
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2024-08-16 17:06:00 -0700

    Start HD ROM

---
+++ HDEEPROM_1.3.s
@@ -0,0 +1,64 @@
+            INCLUDE 'ROMTools/Hardware/Outbound125.s'
+            INCLUDE 'ROMTools/Globals.s'
+            INCLUDE 'ROMTools/CommonConst.s'
+            INCLUDE 'ROMTools/TrapMacros.s'
+
+            org     $F80000
+            dc.l    TROMCode
+            dc.l    ColdEntry
+SEPatchTbl:
+            dc.w    $401C68-BaseOfROM
+            dc.w    PatchMinorStartTest-PtchROMBase
+            dc.w    $401CC0-BaseOfROM
+            dc.w    PatchMinorStartTest2-PtchROMBase
+            dc.w    $401D10-BaseOfROM
+            dc.w    PatchLoadExceptionVectors-PtchROMBase
+            dc.w    $400050-BaseOfROM
+            dc.w    InitPatch-PtchROMBase
+            dc.w    $4029F8-BaseOfROM
+            dc.w    PatchBeep-PtchROMBase
+            dc.w    $4000D2-BaseOfROM
+            dc.w    PatchBootRetry-PtchROMBase
+            dc.w    $400358-BaseOfROM
+            dc.w    PatchGetPRAM-PtchROMBase
+            dc.w    $4001A0-BaseOfROM
+            dc.w    PatchSetupSysAppZone-PtchROMBase
+            dc.w    $4000FE-BaseOfROM
+            dc.w    PatchBootRetry2-PtchROMBase
+            dc.w    $40331E-BaseOfROM
+            dc.w    PatchInitADB-PtchROMBase
+            dc.w    $400786-BaseOfROM
+            dc.w    PatchInitIOMgr-PtchROMBase
+            dc.w    $400790-BaseOfROM
+            dc.w    PatchInitIOMgr2-PtchROMBase
+            dc.w    $4001DA-BaseOfROM
+            dc.w    PatchDrawBeepScreen-PtchROMBase
+PlusPatchTbl:
+            dc.w    $400094-BaseOfROM
+            dc.w    PatchPlusBoot-PtchROMBase
+            dc.w    $4001F8-BaseOfROM
+            dc.w    PatchClkNoMem-PtchROMBase
+            dc.w    $4000CE-BaseOfROM
+            dc.w    PatchPlusBoot2-PtchROMBase
+            dc.w    $4003A8-BaseOfROM 
+            dc.w    PatchBootRetry-PtchROMBase
+            dc.w    $400594-BaseOfROM
+            dc.w    PatchCPUFlag-PtchROMBase
+            dc.w    $4005BC-BaseOfROM
+            dc.w    PatchGetPRAM-PtchROMBase
+            dc.w    $400972-BaseOfROM
+            dc.w    PatchGNEFilter-PtchROMBase
+            dc.w    $40087E-BaseOfROM
+            dc.w    PatchPlusInitIOMgr-PtchROMBase
+            dc.w    $400894-BaseOfROM
+            dc.w    PatchInitIOMgr-PtchROMBase
+            dc.w    $4008A2-BaseOfROM
+            dc.w    PatchInitIOMgr2-PtchROMBase
+
+            org     $F80080
+            dc.l    TROMCode
+            dc.l    WarmEntry
+            dc.b    5                               ; Length byte
+            dc.b    "1.3b2"                         ; ROM version string
+            dc.b    0,0,1,3
+
